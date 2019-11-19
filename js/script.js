32cc352fbc7b22ed0ca964d06f67b648390d4231
maks-confessions <ashjones0227@gmail.com>
2019-11-19 00:10:25 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
-document.getElementById('content').style.height="20%";
 var urlb = "https://discordapp.com/api/webhooks/643675566884519946/ZXcf8f7dWyg3t1SH9i5ylQya7mPv0XVqvfIvI1tnKm_ecO9LbIh5jZE0E115o-e08w3S"
 var blank = ['']
+function lower(){
+  var f = content;
+  f.toLowerCase();
+  content.value=f.value;
+}
 function replaceT(obj){
-  var curses=['nigger','Nigger','fag','Fag'];
+  obj.value=obj.value.toLowerCase();
+  var curses=['nigger','fag','faggot'];
   var i=0, c, r;
   while(c=curses[i++]){
     r=new RegExp(c,'g');

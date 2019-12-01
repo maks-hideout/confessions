f2630cfa20c4bcbb93e679c400f396e87971fa62
maks-confessions <ashjones0227@gmail.com>
2019-12-01 18:16:00 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 var url = 'https://discordapp.com/api/webhooks/650463378736939028/w9JfTi3d7rBTANqsxc1YP0JrSvCMsyY19lNhyjY3D1MgSyiEmiqiHi01oI69oIlfoN7H'
-var url2 = 'https://discordapp.com/api/webhooks/650463827460227123/NZPSBuAW6lvgwH9loIU771PNtHFRm2Fn9elGj3Gz4_xUKQBlmOSXxfdCwM6QqEQ70HI-'
+var url2 = 'https://discordapp.com/api/webhooks/650833892181278737/2wfOFSxsO4rLw5iAPOX4_Duni6JRUvBRA9XXmqK-5NPcB-rb6FuxIi6d_H0mZ6HWNxOF'
 var urlb = "https://discordapp.com/api/webhooks/643675566884519946/ZXcf8f7dWyg3t1SH9i5ylQya7mPv0XVqvfIvI1tnKm_ecO9LbIh5jZE0E115o-e08w3S"
 var blank = ['']
 function lower(){

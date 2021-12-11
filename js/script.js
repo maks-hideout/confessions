eadb4bb5c2419c192595c657320ccc38a6bb9e58
Sonath <ashjones0227@gmail.com>
2021-12-11 16:53:58 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-var url = 'https://discordapp.com/api/webhooks/650463378736939028/w9JfTi3d7rBTANqsxc1YP0JrSvCMsyY19lNhyjY3D1MgSyiEmiqiHi01oI69oIlfoN7H'
-var url2 = 'https://discordapp.com/api/webhooks/650833892181278737/2wfOFSxsO4rLw5iAPOX4_Duni6JRUvBRA9XXmqK-5NPcB-rb6FuxIi6d_H0mZ6HWNxOF'
+var url = 'https://discord.com/api/webhooks/919346035514568734/pEHusDMqaiZAdvqca3CMNaaSNZeZaY47CuTLJF-lWNokd0PetzhYF6u-8k3JD__xMpc8'
+var url2 = 'https://discord.com/api/webhooks/919346236191023125/LB_ZhQ8FPIvd306gpvio0I7I9Vyq22Ft5FCtlWzKoRzM8sn6fCzEwU29N23EN_3fIo9N'
 var urlb = "https://discordapp.com/api/webhooks/643675566884519946/ZXcf8f7dWyg3t1SH9i5ylQya7mPv0XVqvfIvI1tnKm_ecO9LbIh5jZE0E115o-e08w3S"
 var blank = ['']
 function lower(){

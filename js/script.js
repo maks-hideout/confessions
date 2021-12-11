5c1959c9a068c4304fde6e11f7c0c5269e85a04c
Sonath <ashjones0227@gmail.com>
2021-12-11 17:15:18 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var url = 'https://discord.com/api/webhooks/919346035514568734/pEHusDMqaiZAdvqca3CMNaaSNZeZaY47CuTLJF-lWNokd0PetzhYF6u-8k3JD__xMpc8'
+var url = 'https://discord.com/api/webhooks/919351241467899904/_yGaWS5AV2XYmhBhGKiRF49wO4DZQ3JYrh7DmjrDBfVg3muN5o527rNfdsCfdw5Lzenf'
 var url2 = 'https://discord.com/api/webhooks/919346236191023125/LB_ZhQ8FPIvd306gpvio0I7I9Vyq22Ft5FCtlWzKoRzM8sn6fCzEwU29N23EN_3fIo9N'
 var urlb = "https://discordapp.com/api/webhooks/643675566884519946/ZXcf8f7dWyg3t1SH9i5ylQya7mPv0XVqvfIvI1tnKm_ecO9LbIh5jZE0E115o-e08w3S"
 var blank = ['']
